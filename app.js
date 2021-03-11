22c90d276018d71049868dc2d88d34ecfddce0ea
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-11 10:18:09 +0100

    js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+
+var size = 50;
+var positionX = 15;
+var positionY = 15;
+var distance = 5;
+
+context.fillStyle = 'rgba(255,165,0,.5)';
+
+for (var i = 0; i < 5; i++) {
+  positionX = 15;
+  for (var rowItem = 0; rowItem <= i; rowItem++) {
+    context.fillRect(positionX, positionY, size, size);
+    positionX += size + distance;
+  }
+  positionY += size + distance;
+}
